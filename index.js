a20177a6145efe3e689ed620294a078b7faedbfb
Er. Amit Kumar <121847237+amit7974@users.noreply.github.com>
2024-05-01 20:09:59 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,42 +1,51 @@
-
 import React from 'react';
-import './style.css';
-const SearchBarCardComponents = ({searchValue, setSearchValue, typeValue, setTypeValue, filterData})=>{
-    
-    const changeSearchHandle = (e)=>{
-        setSearchValue(e.target.value)
-    }
-    const chagneValueHandler = (e)=>{
-        console.log(e.target.value)
-        setTypeValue(e.target.value)
-    }
-    const handleSubmit = (e)=>{
-        e.preventDefault();
-        filterData()
-    }
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+const AboutContainer = ()=>{
+
     return (
         <>
-            <div className='searchBox'>
-                <div className="container">
-                    <h6>Type movie or tv show name to find it</h6>
-                    <div className='checkBoxSec'>
-                        <label htmlFor="tvFind">
-                            <input type="radio" value="tv" onChange={chagneValueHandler} checked={typeValue === 'tv' ? true : false} name="findSeriesType" id="tvFind" />
-                            <span>Tv </span>
-                        </label>
-                        <label htmlFor="seriesFind">
-                            <input type="radio" value="movie" onChange={chagneValueHandler} checked={typeValue === 'movie' ? true : false} name="findSeriesType" id="seriesFind" />
-                            <span>Movies </span>
-                        </label>
-                    </div>
-                    <form onSubmit={handleSubmit}>
-                        <input type="search" value={searchValue} onChange={changeSearchHandle} placeholder="Search here" />
-                        <input type="submit"  value="FIND" />
-                    </form>
-                </div>
-            </div>
+            <Container>
+                <Row>
+                    <Col className='pd50'>
+                        <p>About Page</p>
+                        <p>
+                            Movie API Home Page Url :- https://www.themoviedb.org/
+                        </p>
+                        <p>
+                            Movie API Key url :- https://www.themoviedb.org/settings/api
+                        </p>
+                        <p>
+                            Movie API Document Page Url :- https://www.themoviedb.org/documentation/api
+                        </p>
+                        <p>
+                             React Bootstrap Page Url :- React Bootstrap
+                        </p>
+                        <p>
+                            Bootstrap Icons Page Url  :- https://react-icons.github.io/react-icons/icons?name=bs
+                        </p>
+                        <p>
+                             Axios Page Url :- https://www.npmjs.com/package/axios
+                        </p>
+                        <p>
+                            React Pagination :- https://www.npmjs.com/package/react-paginate
+                        </p>
+                        <p>
+                            React Alice Carouse :- https://www.npmjs.com/package/react-alice-carousel
+                        </p>
+
+                        
+
+                        
+
+
+                    </Col>
+                </Row>
+            </Container>
         </>
     )
 }
 
-export default SearchBarCardComponents;
+export default AboutContainer;
