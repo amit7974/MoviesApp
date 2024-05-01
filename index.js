b7fa4027f5e9c7797ce19eff8365e990f423f67a
Er. Amit Kumar <121847237+amit7974@users.noreply.github.com>
2024-05-01 20:18:07 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,86 +1,35 @@
 
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import CardMoviesComponents from '../../Components/CardMovies';
-import PaginationComponent from '../../Components/Pagination';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import LeftListBarComponent from '../../Components/LeftListBar';
-import useGenres from '../../Hooks/useGenres';
+import FooterComponent from '../Components/Footer';
+import HeaderComponent from '../Components/Header';
+import AboutContainer from '../Container/About';
+import ContactContainer from '../Container/Contact';
+import DetailsContainer from '../Container/Details';
+import HomeContainer from '../Container/Home';
+import MoviesContainer from '../Container/Movies';
+import SearchContainer from '../Container/Search';
+import TvSeriesContainer from '../Container/TVSeries';
+const  RouteComponent = ()=>{
 
-const  TvSeriesContainer = ()=>{
-    const [content, setContent] = useState([]);
-
-    const [genres, setGenres] = useState([]);
-    const [selectedGenres, setSelectedGenres] = useState([]);
-
-    const [pageno, setPageno] = useState(1)
-    const [paginationno, setPaginationno] = useState(0)
-    const API_KEY = process.env.REACT_APP_NOT_SECRET_CODE;
-
-    
-    const genreforURL = useGenres(selectedGenres)
-    const GetDataTrending = async ()=>{
-        
-        const {data} = await axios.get(`https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&page=${pageno}&with_genres=&language=en-US&with_genres=${genreforURL}`)
-        setContent(data.results);
-        setPaginationno(data.total_pages);
-    }
-
-    useEffect(()=>{
-        console.log('Trending Component did mount');
-        GetDataTrending();
-        //eslint-disable-next-line
-    }, [])
-
-    useEffect(()=>{
-        GetDataTrending();
-        //eslint-disable-next-line
-    }, [pageno, genreforURL])
-
-    const handleClick = (number)=>{
-        setPageno(number);
-    }
-    useEffect(()=>{
-        console.log('Trending Component didupdate mount');
-        GetDataTrending();
-        //eslint-disable-next-line
-    }, [pageno])
     return (
-        <main className='homePage'>
-            <Container>
-                <Row>
-                    <Col className='col-12'>
-                        <section>
-                            <h1 className='txtCenter'>Top Trending TV Series</h1>
-                            <h3 className='txtCenter'> For You</h3>
-                        </section>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col className='col-2'>
-                        <LeftListBarComponent genres={genres} selectedGenres={selectedGenres} setSelectedGenres={setSelectedGenres}  setGenres={setGenres} type="tv" setPage={setPageno}/> 
-                    </Col>
-                    <Col className='col-10'>
-                        <Row>
-                                {
-                                    content && content.length > 0 ? content.map((item, index)=>{
-                                        return (<CardMoviesComponents key={index} data={item} mediaType="tv"/>)
-                                    }) : 'Loading ....'
-                                }
-
-                            {
-                                paginationno && paginationno > 1 ? <PaginationComponent maxnum={paginationno} activenum={pageno} handleClick={handleClick}/> : ''
-                            }
-                        </Row>
-                    </Col>
-                    
-                </Row>
-            </Container>
-        </main>
+        <>
+            <BrowserRouter>
+                <HeaderComponent />
+                    <Routes>
+                        <Route path="/" element={<HomeContainer />} />
+                        <Route path='/about' element={<AboutContainer />} />
+                        <Route path="/movies" element={<MoviesContainer />} />
+                        <Route path="/series" element={<TvSeriesContainer />} />
+                        <Route path="/search" element={<SearchContainer />} />
+                        <Route path="/contact" element={<ContactContainer />} />
+                        <Route path="/details/:movieid/:mediatype" element={<DetailsContainer />} />
+                    </Routes>        
+                <FooterComponent />
+            </BrowserRouter>
+        </>
     )
 }
 
-export default TvSeriesContainer;
+export default RouteComponent;
